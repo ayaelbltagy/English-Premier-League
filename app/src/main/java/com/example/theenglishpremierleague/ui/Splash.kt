c05013a26dcab412ad05108d343327c48fae993e
Aya El Beltagy <aya.elbltagy@businessboomers.net>
2022-11-11 20:21:10 +0200

    -splash screen adding to git

---
+++ app/src/main/java/com/example/theenglishpremierleague/ui/Splash.kt
@@ -0,0 +1,23 @@
+package com.example.theenglishpremierleague.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.theenglishpremierleague.MainActivity
+import com.example.theenglishpremierleague.R
+import kotlinx.coroutines.NonCancellable.isCancelled
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashn)
+        Handler(Looper.getMainLooper()).postDelayed({
+            if (!isCancelled) {
+                startActivity(Intent(this@Splash, MainActivity::class.java))
+                finishAffinity()
+            }
+        }, 1000)
+    }
+}
